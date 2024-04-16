8f8322213e732affea1b49cb4eec2fe63e9f9c8f
email <jackvarkees@gmail.con>
2024-04-16 16:00:00 +0530

    jack

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 // document.getElementById('Jack').textContent='Title of Jackvarkees'
 
+// --------------------------------------------------------
 
-var Header= document.getElementById('Jack');
 
-Header.textContent='Varkees project'
+// var Header= document.getElementById('Jack');
 
-console.log(Header);
-    
+// Header.textContent='Varkees project'
+
+// console.log(Header);
+
+// ---------------------------------------------------------------------
+
+// // {creat a Element and append child via js }
+
+// var creat =document.createElement('p');
+
+// creat.textContent='lorem is upper paragrap';
+
+// var header = document.createElement('h1');
+
+// header.textContent='Heading';
+
+// document.body.append(header,creat);
+
+// ----------------------------------------------------------------------------
+
+
+
+
 
 
 
